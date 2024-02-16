5ba94ad92813cf79b938d7ce8d9785900145db7a
Avenez <145536778+Avenez@users.noreply.github.com>
2024-02-16 16:39:12 +0100

    Aggiornamento

--- E_Commerce/Dettagli.aspx.cs
+++ E_Commerce/Dettagli.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -29,6 +30,13 @@ namespace E_Commerce
                 Response.Redirect("Login");
             }
 
+            string idProdottoSum = Request.QueryString["idProdotto"];
+
+            if (idProdottoSum != null)
+            {
+                UpdateCart(idProdotto);
+            }
+
         }
 
         private string GenerateProductHtml(Prodotto prodotto)
@@ -50,11 +58,68 @@ namespace E_Commerce
                                         $"<h2 class=\"display-2 ps-5\">{prodotto.descrizione}</h2>\r\n" +
                                         $"</div>\r\n" +
 
+
                        
                             $"</div>\r\n\r\n" +
+                            $"<a href=\'Dettagli?idProdotto={prodotto.id}&idProdottoSum={prodotto.id}\' class=\"btn btn-primary\"> <i class=\"bi bi-cart-plus\"></i> Add to Cart</a>\r\n" +
                     $"</div>";
         }
 
+        private void UpdateCart(string idProdotto)
+        {
+            //serializer per trasformare in json
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            //lista di oggetti custom "Prodotto" 
+            List<Prodotto> cart;
+
+
+            //se c'è un cookie carrello lo prende e lo deserializza
+            //se non c'è riempie card con una lista di oggetti Prodotto
+            if (Request.Cookies["CART_COOKIE"] != null && Request.Cookies["CART_COOKIE"]["cart"] != null)
+            {
+                string list = Request.Cookies["CART_COOKIE"]["cart"];
+                cart = ser.Deserialize<List<Prodotto>>(list);
+            }
+            else
+            {
+
+                cart = new List<Prodotto>();
+            }
+
+
+            //controllo se il prodotto è già presente nel cookie "cart"
+
+            bool prodottoTrovato = false;
+
+            foreach (Prodotto prodotto in cart)
+            {
+                //Se è presente ne aumento la qta di 1
+                if (prodotto.id.ToString() == idProdotto)
+                {
+                    prodotto.qta += 1;
+                    Response.Write($"Frutto {prodotto.nome} aggiunto con successo");
+                    prodottoTrovato = true;
+
+                    break;
+                }
+            }
+
+            //Se non lo trova allora aggiunge l'oggetto prodotto corrispondente in "ListaProdottiCustom.listaProdotti"
+            //all'interno della lista
+            if (!prodottoTrovato)
+            {
+                cart.Add((Prodotto)ListaProdottiCustom.listaProdotti[int.Parse(idProdotto) - 1]);
+            }
+
+            //alla fine serializza in json "cart" e aggiorno il cookie
+            string cartJson = ser.Serialize(cart);
+            HttpCookie cartCookie = new HttpCookie("CART_COOKIE");
+            cartCookie.Values["cart"] = cartJson;
+            Response.Cookies.Add(cartCookie);
+        }
+
     }
+
+
     
 }
